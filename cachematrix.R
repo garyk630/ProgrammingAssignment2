a847f97f4d673d32295ecdb94454f9321d1de58a
Gary Kao <garyk630@gmail.com>
2015-08-20 00:43:53 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,12 +3,12 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(sqrMat = matrix()) {
-        if (ncol(sqrMat)!=nrow(sqrMat)){
-        message("getting cached data")
-        return(0)
-        }
-        invMat <- NULL
+makeCachedMatrix <- function(sqrMat = matrix()){
+	if (ncol(sqrMat)!=nrow(sqrMat)){
+		message("Bad matrix: not square!!")
+		return(0)
+	}
+	invMat <- NULL
 	get <- function() sqrMat
 	setInv <- function(solved) invMat <<- solved
 	getInv <- function() invMat
@@ -20,15 +20,14 @@ makeCacheMatrix <- function(sqrMat = matrix()) {
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(cachedMat, ...) {
-        invMat <-cacheMat$getInv()
-        if (!is.null(invMat)){
-        message('Getting cached inversed Matrix!')
-        return(invMat)
-        }
-        sqrMat <- cachedMat$get()
+cacheSolve <- function(cachedMat, ...){
+	invMat <- cachedMat$getInv()
+	if (!is.null(invMat)){
+		message('Getting cahced inversed Matrix!')
+		return(invMat)
+	}
+	sqrMat <- cachedMat$get()
 	invMat <- solve(sqrMat,...)
 	cachedMat$setInv(invMat)
 	return(invMat)
-        ## Return a matrix that is the inverse of 'x'
 }
